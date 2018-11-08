dc026f6157f3c636319c04fe88430d01120caed4
X Squad <43920315+AguilasBuldingCode@users.noreply.github.com>
2018-11-07 23:08:26 -0600

    Login correcto

---
+++ app/src/main/java/com/developers/xsquad/youngadvisors/interfaces/MainActivityInterface.java
@@ -0,0 +1,18 @@
+package com.developers.xsquad.youngadvisors.interfaces;
+
+public interface MainActivityInterface {
+    void checkCurrentUser();
+    /*
+    void getUserProfile();
+    void getProviderData();
+    void updateProfile();
+    void updateEmail();
+    void updatePassword();
+    void sendEmailVerification();
+    void sendEmailVerificationWithContinueUrl();
+    void sendPasswordReset();
+    void deleteUser();
+    void reauthenticate();
+    void authWithGithub();
+    */
+}
